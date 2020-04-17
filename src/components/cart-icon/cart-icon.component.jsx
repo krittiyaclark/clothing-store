af8224b85866963bde8f1ccdc7cf7b13d9f02c83
krittiyaclark <krittiyaclark@gmail.com>
2020-04-17 17:08:18 -0400

    added cartIcon and CartDropdown components to the appliction, also implemented cartReducer

---
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { ReactComponent as ShoppingIcon } from '../../assests/shopping-bag.svg';
+
+import './cart-icon.styles.scss';
+
+const CartIcon = ({ toggleCartHidden }) => (
+	<div className='cart-icon' onClick={toggleCartHidden}>
+		<ShoppingIcon className='shopping-icon' />
+		<span className='item-count'>0</span>
+	</div>
+);
+
+const mapDispatchToPRops = (dispatch) => ({
+	toggleCartHidden: () => dispatch(toggleCartHidden()),
+});
+
+export default connect(null, mapDispatchToPRops)(CartIcon);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,12 +4,15 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { Container, Navbar, Nav } from 'react-bootstrap';
-import { ReactComponent as Logo } from '../../assests/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
+import { ReactComponent as Logo } from '../../assests/crown.svg';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
 	<Container>
 		<Navbar sticky='top' expand='lg' className='header'>
 			<Link to='/' className='logo-container'>
@@ -36,14 +39,17 @@ const Header = ({ currentUser }) => (
 							Sign In
 						</Link>
 					)}
+					<CartIcon />
 				</Nav>
+				{hidden ? null : <CartDropdown />}
 			</Navbar.Collapse>
 		</Navbar>
 	</Container>
 );
 
-const mapStateToProps = (state) => ({
-	currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+	currentUser,
+	hidden,
 });
 
 export default connect(mapStateToProps)(Header);
